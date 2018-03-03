5c80a460c8d2b86f5fcd33455b994ab8357b1d1a
michaeldouglas.10.94@gmail.com <aluno@ubuntu-test>
2018-03-03 10:21:32 -0300

    introdução

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,26 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+                    'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/contato", name="contato")
+     */
+    public function contatoAction(Request $request) {
+
+        $nome = $request->get("nome");
+
+        return $this->render('default/contato.html.twig', array("nome" => $nome));
+    }
+
 }
